971d7c052aa81fc71ec9f4576fad972914ce554a
Mateusz Kwasniak <Mateusz.Kwasniak@cybercom.com>
2016-07-21 10:26:45 +0200

    Refactored TimeTraffic class
    Removed needless lines, added const-qualifier to getters
    
    Change-Id: I56b7000096324fb1b15f0628fb40d9dece682b44

--- LTEsimGeneratorGUI/maps/traffic_map/timetraffic/timetraffic.cpp
+++ LTEsimGeneratorGUI/maps/traffic_map/timetraffic/timetraffic.cpp
@@ -1,185 +1,62 @@
 #include "timetraffic.h"
 
-
-
 TimeTraffic::TimeTraffic()
 {
-    tab1_attachRate = 0;
-    tab1_detachRate = 0;
-    tab1_statsRate = 0;
+    tab1_attachRate = tab1_detachRate = tab1_statsRate = 0;
+    tab2_attachRate = tab2_detachRate = tab2_statsRate = 0;
+    tab1_timeTrafficDuration = tab2_timeTrafficDuration = 0;
 
-    tab1_timeTrafficDuration = 0;
-
-
-    tab2_attachRate = 0;
-    tab2_detachRate = 0;
-    tab2_statsRate = 0;
-
-    tab2_timeTrafficDuration = 0;
     tab2_crdelUEPeriod = 0;
 
-    spn_hours1 = 0;
-    spn_hours2 = 0;
-    spn_minutes1 = 0;
-    spn_minutes2 = 0;
-    spn_seconds1 = 0;
-    spn_seconds2 = 0;
+    spn_hours1 = spn_minutes1 = spn_seconds1 = 0;
+    spn_hours2 = spn_minutes2 = spn_seconds2 = 0;
 }
 
 TimeTraffic::~TimeTraffic()
 {
-
 }
 
-QString TimeTraffic::get_tab1_AttachRate()
-{
-    return QString::number(tab1_attachRate);
-}
+QString TimeTraffic::get_tab1_AttachRate() const {return QString::number(tab1_attachRate);}
+void TimeTraffic::set_tab1_AttachRate(QString rate) {tab1_attachRate = rate.toInt();}
 
-void TimeTraffic::set_tab1_AttachRate(QString ar)
-{
-    tab1_attachRate = ar.toInt();
-}
+QString TimeTraffic::get_tab1_DetachRate() const {return QString::number(tab1_detachRate);}
+void TimeTraffic::set_tab1_DetachRate(QString rate) {tab1_detachRate = rate.toInt();}
 
-QString TimeTraffic::get_tab1_DetachRate()
-{
-    return QString::number(tab1_detachRate);
-}
+QString TimeTraffic::get_tab1_StatsRate() const {return QString::number(tab1_statsRate);}
+void TimeTraffic::set_tab1_StatsRate(QString rate) {tab1_statsRate = rate.toInt();}
 
-void TimeTraffic::set_tab1_DetachRate(QString dr)
-{
-    tab1_detachRate = dr.toInt();
-}
+QString TimeTraffic::get_tab2_AttachRate() const {return QString::number(tab2_attachRate);}
+void TimeTraffic::set_tab2_AttachRate(QString rate) {tab2_attachRate = rate.toInt();}
 
-QString TimeTraffic::get_tab1_StatsRate()
-{
-    return QString::number(tab1_statsRate);
-}
+QString TimeTraffic::get_tab2_DetachRate() const {return QString::number(tab2_detachRate);}
+void TimeTraffic::set_tab2_DetachRate(QString rate) {tab2_detachRate = rate.toInt();}
 
-void TimeTraffic::set_tab1_StatsRate(QString sr)
-{
-    tab1_statsRate = sr.toInt();
-}
+QString TimeTraffic::get_tab2_StatsRate() const {return QString::number(tab2_statsRate);}
+void TimeTraffic::set_tab2_StatsRate(QString rate) {tab2_statsRate = rate.toInt();}
 
+QString TimeTraffic::get_tab2_CrDrUEPeriod() const {return QString::number(tab2_crdelUEPeriod);}
+void TimeTraffic::set_tab2_CrDrUEPeriod(QString period) {tab2_crdelUEPeriod = period.toInt();}
 
-QString TimeTraffic::get_tab2_AttachRate()
-{
-    return QString::number(tab2_attachRate);
-}
+int TimeTraffic::getSpn_hours1() const {return spn_hours1;}
+void TimeTraffic::setSpn_hours1(int hours) {spn_hours1 = hours;}
 
-void TimeTraffic::set_tab2_AttachRate(QString ar)
-{
-    tab2_attachRate = ar.toInt();
-}
+int TimeTraffic::getSpn_minutes1() const {return spn_minutes1;}
+void TimeTraffic::setSpn_minutes1(int minutes) {spn_minutes1 = minutes;}
 
-QString TimeTraffic::get_tab2_DetachRate()
-{
-    return QString::number(tab2_detachRate);
-}
+int TimeTraffic::getSpn_seconds1() const {return spn_seconds1;}
+void TimeTraffic::setSpn_seconds1(int seconds) {spn_seconds1 = seconds;}
 
-void TimeTraffic::set_tab2_DetachRate(QString dr)
-{
-    tab2_detachRate = dr.toInt();
-}
+int TimeTraffic::getSpn_hours2() const {return spn_hours2;}
+void TimeTraffic::setSpn_hours2(int hours) {spn_hours2 = hours;}
 
-QString TimeTraffic::get_tab2_StatsRate()
-{
-    return QString::number(tab2_statsRate);
-}
+int TimeTraffic::getSpn_minutes2() const {return spn_minutes2;}
+void TimeTraffic::setSpn_minutes2(int minutes) {spn_minutes2 = minutes;}
 
-void TimeTraffic::set_tab2_StatsRate(QString sr)
-{
-    tab2_statsRate = sr.toInt();
-}
+int TimeTraffic::getSpn_seconds2() const {return spn_seconds2;}
+void TimeTraffic::setSpn_seconds2(int seconds) {spn_seconds2 = seconds;}
 
-QString TimeTraffic::get_tab2_CrDrUEPeriod()
-{
-    return QString::number(tab2_crdelUEPeriod);
-}
+int TimeTraffic::get_tab1_TimeTrafficDuration() const {return tab1_timeTrafficDuration;}
+void TimeTraffic::set_tab1_TimeTrafficDuration(int duration) {tab1_timeTrafficDuration = duration;}
 
-void TimeTraffic::set_tab2_CrDrUEPeriod(QString uep)
-{
-    tab2_crdelUEPeriod = uep.toInt();
-}
-
-int TimeTraffic::getSpn_hours1() const
-{
-    return spn_hours1;
-}
-
-void TimeTraffic::setSpn_hours1(int value)
-{
-    spn_hours1 = value;
-}
-
-int TimeTraffic::getSpn_minutes1() const
-{
-    return spn_minutes1;
-}
-
-void TimeTraffic::setSpn_minutes1(int value)
-{
-    spn_minutes1 = value;
-}
-
-int TimeTraffic::getSpn_seconds1() const
-{
-    return spn_seconds1;
-}
-
-void TimeTraffic::setSpn_seconds1(int value)
-{
-    spn_seconds1 = value;
-}
-
-int TimeTraffic::getSpn_hours2() const
-{
-    return spn_hours2;
-}
-
-int TimeTraffic::getSpn_minutes2() const
-{
-    return spn_minutes2;
-}
-
-void TimeTraffic::setSpn_minutes2(int value)
-{
-    spn_minutes2 = value;
-}
-
-int TimeTraffic::getSpn_seconds2() const
-{
-    return spn_seconds2;
-}
-
-void TimeTraffic::setSpn_seconds2(int value)
-{
-    spn_seconds2 = value;
-}
-
-int TimeTraffic::get_tab2_TimeTrafficDuration() const
-{
-    return tab2_timeTrafficDuration;
-}
-
-void TimeTraffic::set_tab2_TimeTrafficDuration(int value)
-{
-    tab2_timeTrafficDuration = value;
-}
-
-void TimeTraffic::setSpn_hours2(int value)
-{
-    spn_hours2 = value;
-}
-
-int TimeTraffic::get_tab1_TimeTrafficDuration() const
-{
-    return tab1_timeTrafficDuration;
-}
-
-void TimeTraffic::set_tab1_TimeTrafficDuration(int value)
-{
-    tab1_timeTrafficDuration = value;
-}
-
-
+int TimeTraffic::get_tab2_TimeTrafficDuration() const {return tab2_timeTrafficDuration;}
+void TimeTraffic::set_tab2_TimeTrafficDuration(int duration) {tab2_timeTrafficDuration = duration;}
--- LTEsimGeneratorGUI/maps/traffic_map/timetraffic/timetraffic.h
+++ LTEsimGeneratorGUI/maps/traffic_map/timetraffic/timetraffic.h
@@ -8,70 +8,60 @@ public:
     TimeTraffic();
     ~TimeTraffic();
 
-    QString get_tab1_AttachRate();
-    void set_tab1_AttachRate(QString ar);
+    QString get_tab1_AttachRate() const;
+    void set_tab1_AttachRate(QString);
 
-    QString get_tab1_DetachRate();
-    void set_tab1_DetachRate(QString dr);
+    QString get_tab1_DetachRate() const;
+    void set_tab1_DetachRate(QString);
 
-    QString get_tab1_StatsRate();
-    void set_tab1_StatsRate(QString sr);
+    QString get_tab1_StatsRate() const;
+    void set_tab1_StatsRate(QString);
 
-    QString get_tab2_AttachRate();
-    void set_tab2_AttachRate(QString ar);
+    QString get_tab2_AttachRate() const;
+    void set_tab2_AttachRate(QString);
 
-    QString get_tab2_DetachRate();
-    void set_tab2_DetachRate(QString dr);
+    QString get_tab2_DetachRate() const;
+    void set_tab2_DetachRate(QString);
 
-    QString get_tab2_StatsRate();
-    void set_tab2_StatsRate(QString sr);
+    QString get_tab2_StatsRate() const;
+    void set_tab2_StatsRate(QString);
 
-    QString get_tab2_CrDrUEPeriod();
-    void set_tab2_CrDrUEPeriod(QString uep);
-
+    QString get_tab2_CrDrUEPeriod() const;
+    void set_tab2_CrDrUEPeriod(QString);
 
     int getSpn_hours1() const;
-    void setSpn_hours1(int value);
+    void setSpn_hours1(int);
 
     int getSpn_minutes1() const;
-    void setSpn_minutes1(int value);
+    void setSpn_minutes1(int);
 
     int getSpn_seconds1() const;
-    void setSpn_seconds1(int value);
+    void setSpn_seconds1(int);
 
     int getSpn_hours2() const;
-    void setSpn_hours2(int value);
+    void setSpn_hours2(int);
 
     int getSpn_minutes2() const;
-    void setSpn_minutes2(int value);
+    void setSpn_minutes2(int);
 
     int getSpn_seconds2() const;
-    void setSpn_seconds2(int value);
+    void setSpn_seconds2(int);
 
     int get_tab1_TimeTrafficDuration() const;
-    void set_tab1_TimeTrafficDuration(int value);
+    void set_tab1_TimeTrafficDuration(int);
 
     int get_tab2_TimeTrafficDuration() const;
-    void set_tab2_TimeTrafficDuration(int value);
+    void set_tab2_TimeTrafficDuration(int);
 
 private:
-    int tab1_attachRate;
-    int tab1_detachRate;
-    int tab1_statsRate;
-    int tab2_attachRate;
-    int tab2_detachRate;
-    int tab2_statsRate;
+    int tab1_attachRate, tab1_detachRate, tab1_statsRate;
+    int tab2_attachRate, tab2_detachRate, tab2_statsRate;
 
     int tab2_crdelUEPeriod;
 
-    int spn_hours1;
-    int spn_minutes1;
-    int spn_seconds1;
-    int spn_hours2;
-    int spn_minutes2;
-    int spn_seconds2;
-    int tab1_timeTrafficDuration;
-    int tab2_timeTrafficDuration;
+    int spn_hours1, spn_minutes1, spn_seconds1;
+    int spn_hours2, spn_minutes2, spn_seconds2;
+    int tab1_timeTrafficDuration, tab2_timeTrafficDuration;
 };
 
 #endif // TIMETRAFFIC_H
