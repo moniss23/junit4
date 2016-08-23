d4be43ecdcc4ff63c3886699aacd34415be89a7d
Mateusz Kwasniak <Mateusz.Kwasniak@cybercom.com>
2016-08-23 12:02:38 +0200

    Tidied up Data/Handover class
    Change-Id: I9082646d90a2213efc6ad9dd7433ba7cbdb6c109

--- LTEsimGeneratorGUI/Data/Objects/handover.cpp
+++ LTEsimGeneratorGUI/Data/Objects/handover.cpp
@@ -1,125 +1,26 @@
 #include "Data/Objects/handover.h"
-
-#include "QFile"
-#include <QTextStream>
 #include <Maps/Parameters/MapWindow/mapwindow.h>
 
+//extern QStringList parametersFileContentList;
 
-extern QStringList parametersFileContentList;
+Handover::Handover() {}
+Handover::Handover(const QString &name) {area = name;}
 
+Handover::~Handover() {}
 
+QString Handover::getArea() const {return area;}
+QString Handover::getEastBoundary() const {return QString::number(eastBoundary);}
+QString Handover::getWestBoundary() const {return QString::number(westBoundary);}
+QString Handover::getNorthBoundary() const {return QString::number(northBoundary);}
+QString Handover::getSouthBoundary() const {return QString::number(southBoundary);}
 
-Handover::Handover()
-{
-}
-Handover::Handover(QString name){
-    area = name;
-    setParams();
-}
+void Handover::setArea(const QString &name) { area = name; }
+void Handover::setEastBoundary(const QString &east) {eastBoundary=east.toInt();}
+void Handover::setWestBoundary(const QString &west) {westBoundary=west.toInt();}
+void Handover::setNorthBoundary(const QString &north) {northBoundary=north.toInt();}
+void Handover::setSouthBoundary(const QString &south) {southBoundary=south.toInt();}
 
-Handover::~Handover()
-{
-
-}
-QString Handover::getArea(){
-    return area;
-}
-QString Handover::getEastBoundary(){
-    return QString::number(eastBoundary);
-}
-QString Handover::getNorthBoundary(){
-    return QString::number(northBoundary);
-}
-QString Handover::getSouthBoundary(){
-    return QString::number(southBoundary);
-}
-QString Handover::getWestBoundary(){
-    return QString::number(westBoundary);
-}
-void Handover::setEastBoundary(QString e){
-    eastBoundary=e.toInt();
-}
-
-void Handover::setNorthBoundary(QString n){
-    northBoundary=n.toInt();
-}
-void Handover::setSouthBoundary(QString s){
-    southBoundary=s.toInt();
-}
-void Handover::setWestBoundary(QString w){
-    westBoundary=w.toInt();
-}
-
-void Handover::setParams(){
-    QString name_search = ":area => \"" +area + "\""+",";
-    tabParams = new QString [5];
-    for( int i=0; i<parametersFileContentList.size(); i++){
-        if(parametersFileContentList[i].contains(name_search)){
-            tabParams[0] = parametersFileContentList[i].trimmed();
-            tabParams[1] = parametersFileContentList[i+1].trimmed();
-            tabParams[2] = parametersFileContentList[i+2].trimmed();
-            tabParams[3] = parametersFileContentList[i+3].trimmed();
-            tabParams[4] = parametersFileContentList[i+4].trimmed();
-        }
-    }
-    QStringList list;
-    for (int i=0; i<5; i++)
-    {
-        list = tabParams[i].split(" ", QString::SkipEmptyParts);
-        tabParams[i]= list[2];
-        tabParams[i].remove(QChar('"'));
-        tabParams[i].remove(QChar(','));
-        tabParams[i].remove(QChar(','));
-    }
-
-    area = tabParams[0];
-    southBoundary = tabParams[1].toInt();
-    northBoundary = tabParams[2].toInt();
-    westBoundary = tabParams[3].toInt();
-    eastBoundary = tabParams[4].toInt();
-}
-
-
-void Handover::resetParams()
-{
-    QString fileName = ":/RbFiles/parameters.rb";
-    QFile file1(fileName);
-
-    if(!file1.open(QIODevice::ReadOnly | QIODevice::Text))
-        return;              // if coudn't open file: stop loading file
-
-    QString text1="";
-    QTextStream stream(&file1);
-    QList <QString> fileParameters;
-    while(!stream.atEnd()){
-        text1 = stream.readLine();
-        fileParameters << text1.trimmed();
-   }
-    QString name_search = ":area => \"" +area + "\""+",";
-    tabParams = new QString [5];
-    for( int i=0; i<fileParameters.size(); i++){
-        if(fileParameters[i].contains(name_search)){
-            tabParams[0] = fileParameters[i].trimmed();
-            tabParams[1] = fileParameters[i+1].trimmed();
-            tabParams[2] = fileParameters[i+2].trimmed();
-            tabParams[3] = fileParameters[i+3].trimmed();
-            tabParams[4] = fileParameters[i+4].trimmed();
-        }
-    }
-    QStringList list;
-    for (int i=0; i<5; i++)
-    {
-        list = tabParams[i].split(" ", QString::SkipEmptyParts);
-        tabParams[i]= list[2];
-        tabParams[i].remove(QChar('"'));
-        tabParams[i].remove(QChar(','));
-        tabParams[i].remove(QChar(','));
-    }
-
-    area = tabParams[0];
-    southBoundary = tabParams[1].toInt();
-    northBoundary = tabParams[2].toInt();
-    westBoundary = tabParams[3].toInt();
-    eastBoundary = tabParams[4].toInt();
-
-}
+/* TODO: zasypac jak tylko wczytywanie HO z klasy Project bedzie dokonczone */
+void Handover::setParams() {}
+void Handover::resetParams() {}
+/* ------------------------------------------------------------------------ */
--- LTEsimGeneratorGUI/Data/Objects/handover.h
+++ LTEsimGeneratorGUI/Data/Objects/handover.h
@@ -1,22 +1,24 @@
 #ifndef HANDOVER_H
 #define HANDOVER_H
 #include <QString>
-class Handover
-{
+
+class Handover {
 public:
     Handover();
-    Handover(QString name);
+    Handover(const QString &name);
     ~Handover();
-    QString getArea();
-    QString getSouthBoundary();
-    QString getNorthBoundary();
-    QString getWestBoundary();
-    QString getEastBoundary();
 
-    void setSouthBoundary(QString s);
-    void setNorthBoundary(QString n);
-    void setWestBoundary(QString w);
-    void setEastBoundary(QString e);
+    QString getArea() const;
+    QString getSouthBoundary() const;
+    QString getNorthBoundary() const;
+    QString getWestBoundary() const;
+    QString getEastBoundary() const;
+
+    void setArea(const QString &name);
+    void setWestBoundary(const QString &west);
+    void setEastBoundary(const QString &east);
+    void setSouthBoundary(const QString &south);
+    void setNorthBoundary(const QString &north);
 
     void resetParams();
 
@@ -27,7 +29,6 @@ private:
     int westBoundary;
     int eastBoundary;
 
-    QString *tabParams;
     void setParams();
 };
 
