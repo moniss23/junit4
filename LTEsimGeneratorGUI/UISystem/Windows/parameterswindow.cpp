5d56302c3fdf329d2c998aaab5226edde88ebc5d
Mateusz Kwasniak <Mateusz.Kwasniak@cybercom.com>
2016-08-25 15:49:22 +0200

    Added the feature to draw Cells on a new cell map widget
    Change-Id: Ia15c56ceb9947b8a2fa6ca45e99d7d5697b0dfa8

--- LTEsimGeneratorGUI/UISystem/Windows/parameterswindow.cpp
+++ LTEsimGeneratorGUI/UISystem/Windows/parameterswindow.cpp
@@ -12,6 +12,7 @@
 #include <UISystem/Helpers/rubysyntaxhighlighter.h>
 #include <Maps/Parameters/MapWindow/mapwindow.h>
 #include <Maps/Traffic/map_traffic.h>
+#include "UISystem/Widgets/cellrepresentation.h"
 
 
 QString parametersFileContent;
@@ -390,11 +391,49 @@ void ParametersWindow::msg(const QString &content){
 
 void ParametersWindow::on_showCellMapButton_clicked()
 {
+    const int sceneXMax = 600;
+    const int sceneYMax = 600;
+
+    const int size = 120;
+    const int numberOfCols = 3;
+    const int evenOffset = 40;
+    const int startOffset = 120;
+    const int elementOffset = 160;
+
     QGraphicsView *view = new QGraphicsView();
     QGraphicsScene *scene = new QGraphicsScene();
-    view->setScene(scene);
 
+    // CONFIGURE VIEW
+    view->setScene(scene);
+    view->setMinimumSize(650, 650);
+
+    // CONFIGURE SCENE
+    QPointF coordinateStart(0,590);
+    QPointF coordinateXend(0,0);
+    QPointF coordinateYend(590, 590);
+
+    QLineF line(coordinateStart,coordinateXend);
+    QLineF line2(coordinateStart,coordinateYend);
+    QPen solid(Qt::black, 3, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    QPen solid2(Qt::black, 3, Qt::SolidLine, Qt::SquareCap, Qt::MiterJoin);
+
+    scene->addLine(line,solid);
+    scene->addLine(line2,solid2);
     scene->setSceneRect(-10, -10, 600, 600);
-    view->setMinimumSize(600, 600);
+    scene->setItemIndexMethod(QGraphicsScene::NoIndex);
+
+    // TODO: jak kazdy Project bedzie mial uzupelniony QVector<Cell> zamienic na
+    // for(int i=0; i<currentProject.cells.size(); ++i) {
+    for(int i=0; i<9; ++i) {
+        int row = i / numberOfCols;
+        int col = i % numberOfCols;
+        int evenRow = row % 2;
+
+        CellRepresentation *node = new CellRepresentation();
+        scene->addItem(node);
+        node->setPos(evenRow*evenOffset+col*elementOffset+startOffset,
+                     sceneYMax - startOffset - (elementOffset * row));
+    }
+
     view->show();
 }
