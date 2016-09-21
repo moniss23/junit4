832a5fa73188dc479cb1b28ba92f990c99aba430
Dawid Urbaniak <Dawid.Urbaniak@cybercom.com>
2016-09-21 13:09:40 +0200

    Added doxygen for TrafficFileData class.
    Change-Id: I4644af20f718a8e1384a15e1407473b25f2f5b00

--- LTEsimGeneratorGUI/Data/Objects/trafficfiledata.h
+++ LTEsimGeneratorGUI/Data/Objects/trafficfiledata.h
@@ -10,21 +10,30 @@
 #include "Data/TrafficSettings/tuningTrafficData.h"
 #include "Data/TrafficSettings/timeData.h"
 
-
+/**
+ * @brief The TrafficFileData class contains data that is needed to generate traffic script
+ */
 class TrafficFileData : public SerializeInterface
 {
 public:
+    /**
+     * @brief TrafficFileData default constructor
+     */
     TrafficFileData();
 
-    QString content;
-    QString filename;
-    StatisticsData statisticsData;
-    QVector<UEData> userEquipments;
-    QVector<CustomModelSettings> customModels;
-    TuningTrafficData tuningTrafficData;
-    TimeData timeData;
-    static const int cmAmount = 10;
-    bool cmUsed[cmAmount];
+    /***********************************************
+     *  WINDOW PUBLIC API
+     ***********************************************/
+
+    QString content;                            ///< traffic script content
+    QString filename;                           ///< traffic file name
+    StatisticsData statisticsData;              ///< local copy of statistics Data
+    QVector<UEData> userEquipments;             ///< vector of ue (default 0)
+    QVector<CustomModelSettings> customModels;  ///< vector of customModels (default 10)
+    TuningTrafficData tuningTrafficData;        ///< local copy of tunningtraffic data
+    TimeData timeData;                          ///< local copy of time data
+    static const int cmAmount = 10;             ///< Amount of custom models
+    bool cmUsed[cmAmount];                      ///< table of used customModels
 
     /****************************
      * SERIALIZATION INTERFACE  *
@@ -32,8 +41,20 @@ public:
 public:
     ~TrafficFileData() {}
 
+    /**
+     * @brief getElementType is a API method to recognise the object by QString
+     * @return QString("TrafficFileData")
+     */
     virtual QString getElementType() const;
+    /**
+     * @brief serializeData is an inherited method that serializes this class to QByteArray
+     * @return QByteArray with serialized object of the class
+     */
     virtual QByteArray serializeData();
+    /**
+     * @brief deserializeData ia an inherited method that deserializes it's parameter to fill the fields in this class' instance
+     * @param rawData is QByteArray that contains all the data needed to creata an instance of trafficFileData class
+     */
     virtual void deserializeData(const QByteArray &rawData);
 };
 
