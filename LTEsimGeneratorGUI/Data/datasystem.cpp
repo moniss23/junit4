88fa12638af188d9527f49c15f06949eca1d620b
Pawel Szczerba <Pawel.Szczerba@cybercom.com>
2016-09-21 08:02:07 +0200

    Changing QTime for time.h in datasystem.cpp.
    Change-Id: I2195654b188829b634ac992d07da7a5887a3b4f8

--- LTEsimGeneratorGUI/Data/datasystem.cpp
+++ LTEsimGeneratorGUI/Data/datasystem.cpp
@@ -3,7 +3,7 @@
 #include <QDir>
 #include <QTextStream>
 #include <QSet>
-#include <QTime>
+#include <time.h>
 #include "Data/Managers/filemanager.h"
 #include "Data/Managers/scriptparsermanager.h"
 #include "Data/ProjectSettings/Helpers/mapparser.h"
