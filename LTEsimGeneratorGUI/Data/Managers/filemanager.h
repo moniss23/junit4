e339b8984813ae5095970e922c7aba99abe4ef2d
Dawid Urbaniak <Dawid.Urbaniak@cybercom.com>
2016-09-21 14:31:03 +0200

    Added doxygen for FileManager class.
    Change-Id: I7fcd8ec70415c319127560c79a76eff60ad2ac00

--- LTEsimGeneratorGUI/Data/Managers/filemanager.h
+++ LTEsimGeneratorGUI/Data/Managers/filemanager.h
@@ -6,17 +6,58 @@
 
 #include "Data/project.h"
 
+/**
+ * @brief The FileManager class is responsible for I/O on files
+ */
 class FileManager {
 public:
+    /**
+     * @brief FileManager default constructor
+     */
     FileManager();
 
-    QByteArray readFromFile(const QString&);
-    void writeToFile(const QString&, const QByteArray&);
-    void generateTrafficScript(Project currentProject, int file_index);
-    void generateParametersScript(QString location, QString projectName, QString content);
-    void generateTrafficScript(QString location, QString projectName, QString content, QString fileName);
+    /***********************************************
+     *   PUBLIC API
+     ***********************************************/
 
+
+    /**
+     * @brief readFromFile reads binary data from a file
+     * @param fileFullPath QString with full path to the file
+     * @return QByteArray with file content
+     */
+    QByteArray readFromFile(const QString &fileFullPath);
+    /**
+     * @brief writeToFile writes binary data to file
+     * @param fileFullPath QString with full path to the file
+     * @param rawData QByteArray with binary data
+     */
+    void writeToFile(const QString &fileFullPath, const QByteArray &rawData);
+    /**
+     * @brief generateParametersScript is used to write parametersFileData content to file
+     * @param location is path to file
+     * @param projectName is current Project name
+     * @param content is content that will be write to file
+     */
+    void generateParametersScript(QString location, QString projectName, QString content);
+    /**
+     * @brief generateTrafficScript is used to write trafficFileData content to file
+     * @param location is path to file
+     * @param projectName is current Project name
+     * @param content is content that will be write to file
+     * @param fileName is current traffic file name
+     */
+    void generateTrafficScript(QString location, QString projectName, QString content, QString fileName);
+    /**
+     * @brief readFileToQString is used to read content of the file
+     * @param filename is file name
+     * @return QString with file content
+     */
     QString readFileToQString(const QString &filename);
+    /**
+     * @brief projectsFileSetup is used to check if projects file exist, and create it if doesn't
+     * @param projectsFilename is projec's file name
+     */
     void projectsFileSetup(const QString &projectsFilename);
 };
 
