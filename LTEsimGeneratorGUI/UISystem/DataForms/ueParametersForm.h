db1b4619a025cdb99d8189cf3b63a21dd8a45e83
Pawel Szczerba <Pawel.Szczerba@cybercom.com>
2016-09-23 08:55:37 +0200

    Added doxygen for ueParameteresForm.
    Change-Id: Ia2b756b2fd7edf603a78a86bff8caed27f2ecd37

--- LTEsimGeneratorGUI/UISystem/DataForms/ueParametersForm.h
+++ LTEsimGeneratorGUI/UISystem/DataForms/ueParametersForm.h
@@ -7,6 +7,9 @@
 namespace Ui {
 class UeParametersForm;
 }
+/**
+ * @brief The UeParametersForm class is responsible for showing and managing UEData.
+ */
 
 class UeParametersForm : public QWidget
 {
@@ -21,12 +24,33 @@ public:
     const static QStringList MOBILITYSTRINGLIST;
     const static QStringList UESTRINGLIST;
 
+    /***********************************************
+     *  WINDOW PUBLIC API
+     ***********************************************/
 public slots:
+    /**
+     * @brief loadAndOpen is used to load data and show window.
+     * @param projectName current project name
+     * @param trafficName current traffic name
+     * @param ueData data that we are working on
+     * @param customModelsList list of custom models that are added as items to PS model behaviour
+     * @param cellsAndHo list of calls and hanovers that are added as items to Area
+     */
     void loadAndOpen(const QString& projectName, const QString& trafficName, const UEData& ueData, const QStringList &customModelsList, const QStringList &cellsAndHo);
 
 signals:
+    /**
+     * @brief saveUEData signal is used to save UE data in project
+     * @param projectName current project name
+     * @param trafficName current traffic name
+     * @param ueDataName name of data taht is going to be saved
+     * @param uedata data that is going to be saved
+     */
     void saveUEData(const QString& projectName, const QString& trafficName, const QString& ueDataName, UEData &uedata);
 
+    /***********************************************
+     *  BINDINGS TO UI BUTTONS AND LISTS
+     ***********************************************/
 private slots:
     void on_pairNameLineEdit_editingFinished();
 
@@ -54,18 +78,24 @@ private slots:
 
     void on_cancelButton_clicked();
 
+    /***********************************************
+     *  INTERNAL CLASS LOGIC
+     ***********************************************/
 private:
+    /**
+     * @brief refreshUi is used when window needs to refresh state
+     */
     void refreshUi();
 
 private:
-    Ui::UeParametersForm *ui;
+    Ui::UeParametersForm *ui;               ///<GUI form pointer
 
-    QString projectName;
-    QString trafficName;
-    UEData ueData;
+    QString projectName;                    ///<current project name
+    QString trafficName;                    ///<current traffic name
+    UEData ueData;                          ///<local copy of data
 
-    QStringList cellsAndHandovers;
-    QStringList customModelsList;
+    QStringList cellsAndHandovers;          ///<calls and handovers list
+    QStringList customModelsList;           ///<custom models list
 };
 
 #endif // UEPARAMETERSMANAGER_H
