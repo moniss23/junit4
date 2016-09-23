5ea19220eebf5a992bcfdd31a1c1e0695434114b
Pawel Szczerba <Pawel.Szczerba@cybercom.com>
2016-09-23 08:33:45 +0200

    Added doxygen for generalConfigurationParameters.
    Change-Id: I82ce951e62015686ea4c00dcdab4469bd1eaaf3e

--- LTEsimGeneratorGUI/UISystem/DataForms/generalconfigurationform.h
+++ LTEsimGeneratorGUI/UISystem/DataForms/generalconfigurationform.h
@@ -8,6 +8,9 @@
 namespace Ui {
 class GeneralConfiguration;
 }
+/**
+ * @brief The GeneralConfiguration class is responsible for showing and managing generationconfigurationform data.
+ */
 
 class GeneralConfiguration : public QWidget
 {
@@ -17,13 +20,29 @@ public:
     explicit GeneralConfiguration(QWidget *parent = 0);
     ~GeneralConfiguration();
 
+    /***********************************************
+     *  WINDOW PUBLIC API
+     ***********************************************/
 public slots:
+    /**
+     * @brief loadAndOpen is used to load data and show window
+     * @param generalConfigurationPrameters is local coppy of data
+     * @param projectName is current project name
+     * @param enable is used to disable editing in traffic map
+     */
     void loadAndOpen(const GeneralConfigurationParameters &generalConfigurationPrameters, const QString &projectName, bool enable);
 
 signals:
-
+    /**
+     * @brief updateGeneralConfiguration signal is used to update generalConfigurationPrameters in project
+     * @param generalConfigurationParameters data that is going to be updated
+     * @param projectName current project name
+     */
     void updateGeneralConfiguration(const GeneralConfigurationParameters &generalConfigurationParameters, const QString &projectName);
 
+    /***********************************************
+     *  BINDINGS TO UI BUTTONS AND LISTS
+     ***********************************************/
 private slots:
 
     void on_restoreButton_clicked();
@@ -32,14 +51,27 @@ private slots:
 
     void on_cancelButton_clicked();
 
+    /***********************************************
+     *  INTERNAL CLASS LOGIC
+     ***********************************************/
 private:
+    /**
+     * @brief updateUi is used to set ui fields from GeneralConfigurationParameters data
+     */
     void updateUi();
+    /**
+     * @brief getParameters is used to get GeneralConfigurationParameters data from ui fields.
+     */
     void getParameters();
+    /**
+     * @brief setReadOnly is used to disabled editing
+     * @param value is bool value of possibility of editing fields
+     */
     void setReadOnly(bool value);
 private:
-    Ui::GeneralConfiguration *ui;
-    GeneralConfigurationParameters generalConfigurationParameters;
-    QString projectName;
+    Ui::GeneralConfiguration *ui;                                   //<<< GUI form pointer
+    GeneralConfigurationParameters generalConfigurationParameters;  //<<< Local copy of data
+    QString projectName;                                            //<<< Current Project name
 };
 
 #endif // GENERALCONFIGURATION_H
