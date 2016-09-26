adf69d7755019c934ad3e2abadf0117dd4228cff
Mateusz Kwasniak <Mateusz.Kwasniak@cybercom.com>
2016-09-26 18:49:19 +0200

    Created Doxygen for RenameDialog class
    Change-Id: I8fa8a982bfb1ef2e2420a19a7a8a777415b1e4b7

--- LTEsimGeneratorGUI/UISystem/Windows/renamedialog.h
+++ LTEsimGeneratorGUI/UISystem/Windows/renamedialog.h
@@ -7,14 +7,13 @@
 
 #include "Data/project.h"
 
-/**
- * It's a UI which asks user for new name for RB file
- */
-
 namespace Ui {
 class RenameDialog;
 }
 
+/**
+ * @brief The RenameDialog class is an UI which asks user for new name for RB file
+ */
 class RenameDialog : public QDialog
 {
     Q_OBJECT
@@ -24,30 +23,47 @@ public:
     ~RenameDialog();
 
     virtual void accept();
-
     QString getRbFileExt() const;
 
+    /***********************************************
+     *  WINDOW PUBLIC API
+     ***********************************************/
 signals:
+    /**
+     * @brief changedFilename is emited when new filename for script has been chosen
+     * @param newFilename new filename
+     * @param oldFilename old filename
+     * @param projectName name of a project that holds modified file
+     */
     void changedFilename(const QString& newFilename, const QString& oldFilename, const QString& projectName);
 
 public slots:
+    /**
+     * @brief initWindow initializes RenameDialog with current filename and project's name
+     * @param filename current (old) filename
+     * @param projectName name of a project that holds file to be modified
+     */
     void initWindow(const QString &filename, const QString& projectName);
 
+    /***********************************************
+     *  BINDINGS TO UI BUTTONS                     *
+     ***********************************************/
 private slots:
     void on_buttonBox_rejected();
-
-private:
     void on_buttonBox_accepted();
 
+    /***********************************************
+     *  INTERNAL CLASS LOGIC
+     ***********************************************/
 private:
-    Ui::RenameDialog *ui;
-    QString currentProjectName;      ///< current project name
+    Ui::RenameDialog    *ui;                                    ///< RenameDialog's UI
+    QString             currentProjectName;                     ///< Current project name
 
-    const QString rbFileExt = ".rb"; ///< Extension for rb files.
+    const QString       rbFileExt = ".rb";                      ///< Extension for Ruby files.
 
-    const QString         fileNamePattern = "^[\\w\\s-.]{1,50}$"; ///< Regular expression rules
-    QRegExp               fileNameRegExp;    ///< Regular expression used in name validator
-    QRegExpValidator      fileNameValidator; ///< Validator used to check project names
+    const QString       fileNamePattern = "^[\\w\\s-.]{1,50}$"; ///< Regular expression rules
+    QRegExp             fileNameRegExp;                         ///< RegExp used in name validator
+    QRegExpValidator    fileNameValidator;                      ///< Validator used to check filenames
 };
 
 #endif // RENAMEDIALOG_H
