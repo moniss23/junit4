64b8ea97913d38eb7fcbc771880758bcc31a6d07
Pawel Szczerba <Pawel.Szczerba@cybercom.com>
2016-10-04 09:02:33 +0200

    Added doxygen comments to scriptparsermanager.
    Change-Id: Iaff15503f59cd2a251a1c1195205d9538156e42d

--- LTEsimGeneratorGUI/Data/Managers/scriptparsermanager.h
+++ LTEsimGeneratorGUI/Data/Managers/scriptparsermanager.h
@@ -7,73 +7,160 @@
 #include "Data/project.h"
 
 /**
- * @brief The ScriptParserManager class is responsible for parsing scripts for our application
+ * @brief The ScriptParserManager class is responsible for parsing script
  */
 class ScriptParserManager
 {
 public:
+    /**
+     * @brief ScriptParserManager default constructor and destrucor
+     */
     ScriptParserManager();
     ~ScriptParserManager();
 
-    // Methods below try to fetch specified object's fields from given QStringList
-    QVector<Cell> getCellsFromScript(const QStringList scriptContent);
-    QVector<Center> getCentersFromScript(const QStringList scriptContent);
-    QVector<Handover> getHandoversFromScript(const QStringList scriptContent);
-    SgwSettings getSgwSettings(const QStringList scriptContentLines);
-    MmeSettings getMmeSettings(const QStringList scriptContentLines);
-    UeParameters getUeParameters(const QStringList &scriptContentLines);
-    UBSimSettings getUBSimSettings(const QStringList scriptContentLines);
-    PagingSettings getPagingSettings(const QStringList scriptContentLines);
-    MapRange getMapRange(const QStringList scriptContentLines, Project &project);
-    UCToolSettings getUCToolSettingsFromScript(const QStringList scriptContent);
-    ChannelModelSettings getChannelModelSettingsFromScript(const QStringList content);
-    DataGeneratorSettings getDataGeneratorSettingsFromScript(const QStringList scriptContentLines);
-    GeneralConfigurationParameters getGeneralConfigurationSettings(const QStringList scriptContentLines);
-
+    /***********************************************
+     *   PUBLIC API
+     ***********************************************/
     /**
-     * @brief matchCellsToCenters matches each Cell object to its Center (matching by names)
-     * @param cells
-     * @param centers
-     * @return vector of pairs <Cell, Center>
+     * @brief parseFromScript parses from script
+     * @param scriptContent QString with full script
+     * @param project holds data about the project
+     */
+    void parseFromScript(const QString &scriptContent, Project &project);
+    /**
+     * @brief getSgwSettings is parsing SgwSettings from script
+     * @param scriptContentLines script content in QStringList
+     * @return SgwSettings with data from parsing script
+     */
+    SgwSettings getSgwSettings(const QStringList scriptContentLines);
+    /**
+     * @brief getUCToolSettingsFromScript is parsing UCToolSettings from script
+     * @param scriptContent script content in QStringList
+     * @return UCToolSettings with data from parsing script
+     */
+    UCToolSettings getUCToolSettingsFromScript(const QStringList scriptContent);
+    /**
+     * @brief getChannelModelSettingsFromScript is parsing ChannelModelSettings from script
+     * @param content script content in QStringList
+     * @return ChannelModelSettings with data from parsing script
+     */
+    ChannelModelSettings getChannelModelSettingsFromScript(const QStringList content);
+    /**
+     * @brief getDataGeneratorSettingsFromScript is parsing DataGeneratorSettings from script
+     * @param scriptContentLines script content in QStringList
+     * @return DataGeneratorSettings with data from parsing script
+     */
+    DataGeneratorSettings getDataGeneratorSettingsFromScript(const QStringList scriptContentLines);
+    /**
+     * @brief getMmeSettings is parsing MmeSettings from script
+     * @param scriptContentLines script content in QStringList
+     * @return MmeSettings with data from parsing script
+     */
+    MmeSettings getMmeSettings(const QStringList scriptContentLines);
+    /**
+     * @brief getPagingSettings is parsing PagingSettings from script
+     * @param scriptContentLines script content in QStringList
+     * @return PagingSettings with data from parsing script
+     */
+    PagingSettings getPagingSettings(const QStringList scriptContentLines);
+    /**
+     * @brief getUBSimSettings is parsing UBSimSettings from script
+     * @param scriptContentLines script content in QStringList
+     * @return UBSimSettings with data from parsing script
+     */
+    UBSimSettings getUBSimSettings(const QStringList scriptContentLines);
+    /**
+     * @brief getGeneralConfigurationSettings is parsing GeneralConfigurationParameters from script
+     * @param scriptContentLines script content in QStringList
+     * @return GeneralConfigurationParameters with data from parsing script
+     */
+    GeneralConfigurationParameters getGeneralConfigurationSettings(const QStringList scriptContentLines);
+    /**
+     * @brief getMapRange is parsing MapRange from script
+     * @param scriptContentLines script content in QStringList
+     * @param project holds data about the project
+     * @return MapRange with data from parsing script
+     */
+    MapRange getMapRange(const QStringList scriptContentLines, Project &project);
+    /**
+     * @brief getUeParameters is parsing UeParameters from script
+     * @param scriptContentLines script content in QStringList
+     * @return UeParameters with data from parsing script
+     */
+    UeParameters getUeParameters(const QStringList &scriptContentLines);
+    /**
+     * @brief GenerateParametersQString is generating parameters.rb
+     * @param projectholds data about the project
+     * @return QString with parameters.rb content
+     */
+    QString GenerateParametersQString(Project &project);
+    /**
+     * @brief validateData is used to validate parsed cells and map range data
+     * @param project data about the project
+     * @return QString with error output
+     */
+    QString validateData(const Project &project);
+
+    /***********************************************
+     *  INTERNAL CLASS LOGIC
+     ***********************************************/
+private:
+    /**
+     * @brief getCellsFromScript is getting cells data from script
+     * @param scriptContent script content in QStringList
+     * @return QVector<Cell> - cells data
+     */
+    QVector<Cell> getCellsFromScript(const QStringList scriptContent);
+    /**
+     * @brief getCentersFromScript is getting centers data from script
+     * @param scriptContent script content in QStringList
+     * @return  QVector<Center> - cells data
+     */
+    QVector<Center> getCentersFromScript(const QStringList scriptContent);
+    /**
+     * @brief getHandoversFromScript is getting handovers data from script
+     * @param scriptContent script content in QStringList
+     * @return  QVector<Handover> - cells data
+     */
+    QVector<Handover> getHandoversFromScript(const QStringList scriptContent);
+    /**
+     * @brief matchCellsToCenters is matching cells to centers
+     * @param cells cells to match
+     * @param centers centersto match
+     * @return QVector<QPair<Cell, Center>> vector of pairs cells and centers
      */
     QVector<QPair<Cell, Center>> matchCellsToCenters(auto cells, auto centers);
 
     /**
-     * @brief GenerateParametersQString
-     * @param project
-     * @return Parameters file content (as QString object)
-     */
-    QString GenerateParametersQString(Project &project);
-
-    /**
-     * @brief parseFromScript goes through script, parses each found section and saves it to a project
-     * @param scriptContent
-     * @param project
-     */
-    void parseFromScript(const QString &scriptContent, Project &project);
-
-    /**
-     * @brief findRegexInText
-     * @param pattern
-     * @param text
-     * @param pos
-     * @return string catched by reg. exp. pattern (its pos-th item) in text
+     * @brief findRegexInText is finding patters in text
+     * @param pattern patteres to find
+     * @param text text to search pattern in
+     * @param pos position to start searching pattern
+     * @return QString that contains pattern
      */
     QString findRegexInText(QString pattern, const QString &text, int pos);
 
-    /********************
-     * DATA VALIDATION  *
-     ********************/
-    /**
-     * @brief validateData executes validation methods in order to check if parsed script is complete
-     * @param project
-     * @return error message informing which sections could not be found
-     */
-    QString validateData(const Project &project);
 
+    // ******** DATA VALIDATION ********* //
+    /**
+     * @brief validateCells is used to validate parsed cells
+     * @param project data about the project
+     * @return bool if data are correct
+     */
     bool validateCells(const Project &project);
+    /**
+     * @brief validateHandovers is used to validate parsed handovers
+     * @param project data about the project
+     * @return  bool if data are correct
+     */
     bool validateHandovers(const Project &project);
+    /**
+     * @brief validateMapRange is used to validate parsed map range
+     * @param project data about the project
+     * @return  bool if data are correct
+     */
     bool validateMapRange(const Project &project);
+    // ******** DATA VALIDATION ********* //
 
 };
 
